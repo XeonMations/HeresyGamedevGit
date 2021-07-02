e18862d4bb619106c8e84efc2379893b270a2d80
WilloGitHub <77099834+WilloGitHub@users.noreply.github.com>
2021-07-02 16:15:18 +1000

    Further progress on the Parkour Controller

--- HeresyGamedev/Assets/Willoguns/Scripts/ParkourPlayerMovement.cs
+++ HeresyGamedev/Assets/Willoguns/Scripts/ParkourPlayerMovement.cs
@@ -7,14 +7,26 @@ public class ParkourPlayerMovement : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private float moveSpeed = 6f;
     [SerializeField] private float movementMultiplier = 10f;
+    [SerializeField] private float airMultiplier = 0.4f;
     [SerializeField] private float rbDrag = 6f;
+    [SerializeField] private Rigidbody rb;
+
+    [Header("KeyBinds")]
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Jumping")]
+    [SerializeField] private float jumpForce = 5f;
+
+    [Header("Drag")]
+    [SerializeField] private float groundDrag = 6f;
+    [SerializeField] private float airDrag = 2f;
 
     private float horizontalMovement;
     private float verticalMovement;
+    private float playerHeight = 2f;
 
+    private bool isGrounded;
     private Vector3 moveDirection;
-
-    [SerializeField] private Rigidbody rb;
 
     private void Start()
     {
@@ -23,8 +35,15 @@ public class ParkourPlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight / 2 + 0.1f);
+
         MyInput();
         ControlDrag();
+
+        if(Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            Jump();
+        }
     }
 
     private void MyInput()
@@ -35,13 +54,32 @@ public class ParkourPlayerMovement : MonoBehaviour
         moveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
     }
 
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
     private void ControlDrag()
     {
-        rb.drag = rbDrag;
+        if(isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        if(isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        }
+        else if(!isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier * airMultiplier, ForceMode.Acceleration);
+        }
     }
 }
